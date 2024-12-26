753e8686d4d4c180f0d60693b336b64ababa1896
zyxucp <286513187@qq.com>
2024-12-26 17:41:53 +0800

    update

---
+++ src/AIShell/Pages/Shell/ShellPage.razor.cs
@@ -0,0 +1,40 @@
+﻿using AIShell.Domain.Repositories;
+using AntDesign;
+using BlazorComponents.Terminal;
+using Microsoft.Identity.Client;
+
+namespace AIShell.Pages.Shell
+{
+    public partial class ShellPage
+    {
+        private BlazorTerminal blazorTerminal = new BlazorTerminal();
+        private TerminalParagraph para;
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            blazorTerminal.ConfigPrompt(new CommandPrompt()
+            {
+                Name = "visitor",
+                Host = "blazor.terminal",
+                Path = "~",
+                Separator1 = '@',
+                Separator2 = ':',
+                Prompt = '#',
+                AnswerPrompt = ">"
+            });
+        }
+        async void commandEnter(TerminalEventArgs evenArgs)
+        {
+            //your code
+        }
+
+        //OnAnswerEnter event
+        async void answerEnter(TerminalEventArgs evenArgs)
+        {
+            //your code
+        }
+
+    }
+}
+    
+
